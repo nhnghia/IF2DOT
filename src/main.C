0d55c752301d970ada73ebd05b64566dba2ab9e3
nhnghia <nhnghia@me.com>
2015-02-19 22:14:51 +0100

    IF 2 Java simulator

--- src/main.C
+++ src/main.C
@@ -10,15 +10,16 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
-
+#include <fstream>
 #include <string>
 #include <algorithm>
-
 #include "model.h"
 
 using namespace std;
 
-
+/**
+ * This just is a way to look into protected variables of IfSignalroute
+ */
 class State: public IfNextstateAction{
 	public:
 	char *getStateName(){
@@ -28,13 +29,30 @@ class State: public IfNextstateAction{
 	}
 };
 
+
+class SignalRoute : public IfSignalroute{
+	public:
+	char *getFromName(){
+		return m_pFromName;
+	}
+	char *getToName(){
+		return m_pToName;
+	}
+	int GetUpper(){
+		return m_iUpper;
+	}
+};
+
+
+void print_ok(){
+	printf("\n      ==>   OK");
+}
+
 FILE *output;
-char *output_folder;
-bool isOnlyState;
 int tranID;
 
 //system name
-const char *sysName;
+const char *systemName;
 
 //IfRangeType ==> int
 char *range_types[20];
@@ -53,6 +71,19 @@ char* replaceAll(const char *txt, char* c_from, char *c_to){
 		start_pos = s_txt.find(s_from, start_pos);
 	}
 	return strdup(s_txt.c_str());
+}
+
+/**
+ * get a char* containing number of tab
+ * @param number_of_tab
+ * @return
+ */
+char * get_space(int number_of_tab){
+	char *buf = (char *)malloc(sizeof(char) * (number_of_tab + 1));
+	strcpy(buf, "\n");
+	for (int i=0; i<number_of_tab; i++)
+		sprintf(buf, "%s\t", buf);
+	return buf;
 }
 
 /*
@@ -130,11 +161,9 @@ void printBody(const char *space, IfBlockStatement *body){
 				IfCallAction *ca = (IfCallAction *) act;
 				fprintf(output, "%s%s = %s(", space, Dump(ca->GetLhs()), ca->GetProcedure()->GetName());
 				IfList<IfExpression> *exp = ca->GetParameters();
-				int n = exp->GetSize();
+				int n = exp->GetCount();
 				for (int i=0; i<n; i++){
 					IfExpression *e = exp->GetAt(i);
-					if (e == NULL)
-						continue;
 					if (i == 0)
 						fprintf(output, "%s", Dump(e));
 					else
@@ -144,14 +173,11 @@ void printBody(const char *space, IfBlockStatement *body){
 			}
 			else if (act->IsOutput()){
 				IfInputAction *in = (IfInputAction *) act;
-
-				fprintf(output, "%soutput(\"%s\"", space, in->GetSignal()->GetName());
+				fprintf(output, "%soutput(%s.class", space, in->GetSignal()->GetName());
 				IfList<IfExpression> *vars = in->GetParameters();
-				int n = vars->GetSize();
+				int n = vars->GetCount();
 				for (int i=0; i<n; i++){
 					IfExpression *v = vars->GetAt(i);
-					if (v == NULL)
-						continue;
 					fprintf(output, ", %s", Dump(v));
 				}
 				fprintf(output, ");");
@@ -174,7 +200,7 @@ char *getOutput(IfBlockStatement *body){
 	IfList<IfBasicStatement> *statements = body->GetStatements();
 	if (statements == NULL)
 		return "";
-	int n=statements->GetSize();
+	int n=statements->GetCount();
 	for (int i=0; i<n; i++){
 		IfBasicStatement *s = statements->GetAt(i);
 		if (s == NULL)
@@ -206,6 +232,8 @@ char *getOutput(IfBlockStatement *body){
  * @return
  */
 char * get_type(IfType *type){
+	if (type == NULL)
+		return "";
 	if (type->IsString())
 		return "String";
 	else if (type->IsBasic()){
@@ -218,17 +246,17 @@ char * get_type(IfType *type){
 	//integer
 	char *name = type->GetName();
 	if (strcmp(name, "integer") == 0)
-		return "int";
+		return "Integer";
 	if (strcmp(name, "clock") == 0)
 			return "Integer";
 	if (strcmp(name, "pid") == 0)
-			return strdup(sysName);
+			return strdup(systemName);
 	if (strcmp(name, "bool") == 0)
 				return "boolean";
 	//range
 	for (int i=0; i<range_type_number; i++){
 		if (strcmp(name, range_types[i]) == 0)
-			return "int";
+			return "Integer";
 	}
 	return name;
 }
@@ -252,16 +280,24 @@ const char *getTarget (IfTransition *tran){
 	return "_";
 }
 
-int print_input(const char *space, IfAction *input){
+char * get_input(const char *space, IfAction *input){
 	if (input == NULL)
-		return 0;
+		return NULL;
 
 	if (input->IsInput()){
 		IfInputAction *in = (IfInputAction *) input;
-
-		fprintf(output, "%sinput(\"%s\"", space, in->GetSignal()->GetName());
+		//TODO supporint currently only one parameter
 		IfList<IfExpression> *vars = in->GetParameters();
-		int n = vars->GetSize();
+		IfExpression *ex = vars->GetAt(0);
+		char *type = get_type(in->GetSignal()->GetParameters()->GetAt(0));
+
+		char  buff[250];
+		snprintf(buff, sizeof(buff), "%s%s = (%s) getInput(%s.class, %s.class)", space, Dump(ex),
+					type,
+					in->GetSignal()->GetName(), type);
+		return buff;
+		/*IfList<IfExpression> *vars = in->GetParameters();
+		int n = vars->GetCount();
 		for (int i=0; i<n; i++){
 			IfExpression *v = vars->GetAt(i);
 			if (v == NULL)
@@ -269,8 +305,9 @@ int print_input(const char *space, IfAction *input){
 			fprintf(output, ", %s", Dump(v));
 		}
 		fprintf(output, ");");
+		*/
 	}
-	return 1;
+	return NULL;
 }
 
 
@@ -294,7 +331,7 @@ int print_tran(IfState *src, IfTransition *tran){
 	inStr = replaceAll(inStr, "input ", "");
 	char *outStr = getOutput(tran->GetBody());
 
-	fprintf(output, "\n		//transition id=\"tr%d\" dst=\"%s\" input=\"%s\" output=\"%s\"",
+	fprintf(output, "\n			//transition id=\"tr%d\" dst=\"%s\" input=\"%s\" output=\"%s\"",
 			(++tranID),
 			dst,
 			inStr,
@@ -303,26 +340,35 @@ int print_tran(IfState *src, IfTransition *tran){
 	bool isIf = false;
 	char *whenStr = Dump(tran->GetWhen());
 	if (strcmp(whenStr, "")){
-		fprintf(output, "\n		if (%s) {", replaceAll(whenStr, " = ", "=="));
+		fprintf(output, "\n			if (%s) {", replaceAll(whenStr, " = ", "=="));
 		isIf = true;
 	}
 	char *providedStr = Dump(tran->GetProvided());
 
 	if (strcmp(providedStr, "")){
-		fprintf(output, "\n		if (%s) {", replaceAll(providedStr, " = ", "=="));
+		fprintf(output, "\n			if (%s) {", replaceAll(providedStr, " = ", "=="));
 		isIf = true;
 	}
 
-	char *space = "\n		";
+	int number_of_tab = 3;
 	if (isIf)
-		space = "\n			";
+		number_of_tab ++ ;
 
-	print_input(space, tran->GetInput());
-	printBody(space, tran->GetBody());
-	fprintf(output, "%snextState(\"%s\");", space, dst);
+	char  *buf = get_input("", tran->GetInput());
+
+	if (buf != NULL){
+		fprintf(output, "%sif ((%s) != null) {", get_space(number_of_tab), buf);
+		number_of_tab ++;
+	}
+
+	printBody(get_space(number_of_tab), tran->GetBody());
+	fprintf(output, "%snextState(\"%s\");", get_space(number_of_tab), dst);
+
+	if (buf != NULL)
+		fprintf(output, "%s}", get_space(--number_of_tab));
 
 	if (isIf)
-		fprintf(output, "\n		}");
+		fprintf(output, "\n			}");
 
 	return 1;
 }
@@ -330,9 +376,9 @@ int print_tran(IfState *src, IfTransition *tran){
 
 int print_state(IfState *s){
 	if (s->IsStart()){
-			fprintf(output, "\n\n	public void state_init(){");
+			fprintf(output, "\n\n		public void state_init(){");
 	}else
-		fprintf(output, "\n\n	public void state_%s(){", s->GetName());
+		fprintf(output, "\n\n		public void state_%s(){", s->GetName());
 
 	IfList<IfTransition> *trans = s->GetOutTransitions();
 	int n = trans->GetCount();
@@ -341,11 +387,14 @@ int print_state(IfState *s){
 		print_tran(s, trans->GetAt(i));
 	}
 
-	fprintf(output, "\n	}");
+	fprintf(output, "\n		}");
 	return 1;
 }
 
-int print_variables(char *space, IfList<IfVariable> *variables){
+int print_variables(const char *space, IfList<IfVariable> *variables){
+	if (variables == NULL)
+		return 0;
+
 	int n = variables->GetCount();
 
 	for (int i=0; i<n; i++){
@@ -365,24 +414,24 @@ int print_variables(char *space, IfList<IfVariable> *variables){
 int print_proc_update_clocks(IfList<IfVariable> *variables){
 	int n = variables->GetCount();
 
-	fprintf(output, "\n\n	public void updateClock(int val){");
+	fprintf(output, "\n\n		public void updateClock(int val){");
 
 	for (int i=0; i<n; i++){
 		IfVariable *var = variables->GetAt(i);
 		if (strcmp(var->GetType()->GetName(), "clock") == 0){
-			fprintf(output, "\n		%s = val;", var->GetName());
+			fprintf(output, "\n			%s = val;", var->GetName());
 		}
 	}
-	fprintf(output, "\n	}");
+	fprintf(output, "\n		}");
 	return 1;
 }
 
 int print_procedure(IfProcedure *proc){
 	if (proc == NULL)
 		return 0;
-	fprintf(output, "\n	//procedure\n	public %s %s(", get_type(proc->GetReturn()), proc->GetName());
+	fprintf(output, "\n\n		//procedure\n		public %s %s(", get_type(proc->GetReturn()), proc->GetName());
 	IfList<IfVariable> *vars = proc->GetParameters();
-	int n = vars->GetSize();
+	int n = vars->GetCount();
 	for (int i=0; i<n; i++){
 		IfVariable *v = vars->GetAt(i);
 		if (v == NULL)
@@ -394,32 +443,19 @@ int print_procedure(IfProcedure *proc){
 	}
 	char *str = replaceAll(proc->GetBody(), "{#", "");
 	str = replaceAll(str, "#}", "");
-	fprintf(output, "){\n		%s\n	}", str);
+	fprintf(output, "){\n			%s\n		}", str);
 	return 1;
 }
 
-int print_proc(const char * sysName, IfProcessEntity *proc){
+int print_proc(IfProcessEntity *proc){
 	tranID = 0;
 	const char *procName = proc->GetName();
 
-	int len = strlen(procName) + 5;
-	char *str = new char[len];
+	printf("\n  Write the process [%s]", procName);
 
-	sprintf(str, "%s.java", procName);
-	
-	printf("\n  Write the process [%s] to file [%s]", procName, str);
+	fprintf(output, "\n	//================================\n	public static class %s extends %s{", procName, systemName);
 
-	output = fopen(str, "w");
-
-	if (output == NULL){
-		printf("\n     ==> Error: cannot create file [%s]\n", str);
-		exit(0);
-	}
-
-
-	fprintf(output, "public class %s extends %s{", procName, sysName);
-
-	print_variables("	", proc->GetVariables());
+	print_variables("		", proc->GetVariables());
 
 	//update clock
 	print_proc_update_clocks(proc->GetVariables());
@@ -435,16 +471,16 @@ int print_proc(const char * sysName, IfProcessEntity *proc){
 		print_state(states->GetAt(i));
 	}
 
-	//print other
-	fprintf(output, "\n	public void callState(String stateName){");
+	//print other methods
+	fprintf(output, "\n		public void callState(String stateName){");
 	for (int i=0; i<n; i++){
 		IfState *s = states->GetAt(i);
-		fprintf(output, "\n		if (stateName.equals(\"%s\")) state_%s();", s->GetName(), s->GetName());
+		fprintf(output, "\n			if (stateName.equals(\"%s\")) state_%s();", s->GetName(), s->GetName());
 	}
-	fprintf(output, "\n	}");
+	fprintf(output, "\n		}");
 
-	fprintf(output, "\n}");
-	printf("\n      ==>   OK");
+	fprintf(output, "\n	}");
+	print_ok();
 	return 1;
 }
 
@@ -475,6 +511,7 @@ void print_type_definitions(IfList<IfType> *types){
 	if (types == NULL)
 		return;
 
+	char file[250];
 	int n = types->GetCount();
 	for (int i=0; i<n; i++){
 		IfType *ty = types->GetAt(i);
@@ -502,25 +539,65 @@ void print_type_definitions(IfList<IfType> *types){
 		}
 		else if (ty->IsRecord()){
 			IfRecordType *rec = (IfRecordType *) ty;
-			fprintf(output, "\n	public static class %s{", rec->GetName());
-			print_variables("		",rec->GetFields());
-			fprintf(output, "\n	}");
+			char *name = rec->GetName();
+			//The signals will be changed to record with name starts with t_
+			if (3 <= strlen(name) && strncmp("t_",name,2) != 0 ){
+				fprintf(output, "\n	public static class %s{", name);
+				print_variables("		",rec->GetFields());
+				fprintf(output, "\n	}");
+			}
 		}
 	}
 }
 
+int print_signal_routes(IfList<IfSignalroute> *routes){
+	if (routes == NULL)
+		return 0;
+	int n = routes->GetCount();
+	fprintf(output, "\n\n	//Signals=============================");
+	for (int i=0; i<n; i++){
+		SignalRoute *sr = (SignalRoute*)routes->GetAt(i);
+
+		char *src = sr->getFromName(), *dst = sr->getToName();
+		IfList<IfSignal> *lst = sr->GetSignals();
+		int m = lst->GetCount();
+		for (int j=0; j<m; j++){
+			IfSignal *s = lst->GetAt(j);
+			//TODO currently support only one parameter in signal
+			if (s->GetParameters()->GetAt(0) != NULL)
+				fprintf(output, "\n	public static class %s extends Signal{"
+								"\n		public static String source = \"%s\";"
+								"\n		public static String destination = \"%s\";"
+								"\n		public %s param;"
+								"\n	}",
+								s->GetName(), src, dst,
+								get_type(s->GetParameters()->GetAt(0)));
+			//a signal contains no data, e.g, ACK
+			else
+				fprintf(output, "\n	public static class %s extends Signal{"
+								"\n		public static String source = \"%s\";"
+								"\n		public static String destination = \"%s\";"
+								"\n	}",
+								s->GetName(), src, dst);
+		}
+	}
+	return 1;
+}
+
 int print_system_define(IfSystemEntity *sys){
-	const char *procName = sys->GetName();
-	char *str = new char[strlen(procName) + 5];
-	sprintf(str, "%s.java", procName);
+	fprintf(output, "public class %s extends Simulator{", systemName);
 
-	printf("\n  Write definitions of system [%s] to file [%s]", procName, str);
-	output = fopen(str, "w");
-
-	fprintf(output, "public class %s{", procName);
-	print_constants(sys->GetConstants());
-	print_variables("	", sys->GetVariables());
+	//Data type
 	print_type_definitions(sys->GetTypes());
+
+	print_constants(sys->GetConstants());
+
+	print_variables("	", sys->GetVariables());
+
+	print_signal_routes(sys->GetSignalroutes());
+
+	print_ok();
+	//init
 
 	//class Clock
 	/*fprintf(output, "\n	public class Clock extends Integer{\n		%s\n		 %s\n	}",
@@ -528,14 +605,15 @@ int print_system_define(IfSystemEntity *sys){
 			"public void reset(){\n			this = 0;\n		}");
 	*/
 
-	fprintf(output, "\n	public void nextState(String stateName){}");
-
-	fprintf(output, "\n	public void input(String className, Object var){}");
-
-	fprintf(output, "\n	public void output(String className, Object var){}");
+	IfList<IfProcessEntity> *proc = sys->GetProcesses();
+	int n = proc->GetCount();
+	for (int i=0; i<n; i++){
+		print_proc(proc->GetAt(i));
+	}
 
 	fprintf(output, "\n}");
-	printf("\n      ==>   OK");
+
+
 	return 1;
 }
 
@@ -589,40 +667,42 @@ FILE* copy(FILE *src){
 
 int main(int argc, char * argv[]) {
   
-  if (argc != 2) {
-    fprintf(stderr, "\n Usage: %s ifFileName",argv[0]);
-	 fprintf(stderr, "\n\n");
-    return 0;
-  }
-  
-  FILE *file;
-  file = fopen(argv[1], "r");
-  if (file == NULL){
-	  fprintf(stderr, "Cannot open file: %s", argv[1]);
-	  return 0;
-  }
+	if (argc != 2) {
+		fprintf(stderr, "\n Usage: %s ifFileName", argv[0]);
+		fprintf(stderr, "\n\n");
+		return 0;
+	}
 
-  output_folder = argv[1];
-  //remove .if at the end of fileName
-  output_folder[strlen(output_folder) - 3] = '\0';
+	FILE *file;
+	file = fopen(argv[1], "r");
+	if (file == NULL) {
+		fprintf(stderr, "Cannot open file: %s", argv[1]);
+		return 0;
+	}
 
+	file = copy(file);
 
-  file = copy(file);
- 
+	IfSystemEntity *system = Load(file, NULL, 0);
 
-  IfSystemEntity *system = Load(file, NULL, 0);
-  print_system_define(system);
+	systemName = system->GetName();
+	char *str = (char *) malloc(strlen(systemName) + 5);
 
-  IfList<IfProcessEntity> *proc = system->GetProcesses();
-  sysName = system->GetName();
-  int n = proc->GetCount();
-  for (int i=0; i<n; i++){
-	  print_proc(sysName, proc->GetAt(i));
-  }
-  
-  fclose(file);
-  printf("\n\n");
-  return 1; 
+	//
+	sprintf(str, "%s.java", systemName);
+	printf("\n  Write definitions of system [%s] to file [%s]", systemName, str);
+
+	output = fopen(str, "w");
+
+	if (output == NULL) {
+		printf("\n     ==> Error: cannot create file [%s]\n", str);
+		exit(0);
+	}
+
+	print_system_define(system);
+
+	fclose(file);
+	printf("\n\n");
+	return 1;
 }
 
 
